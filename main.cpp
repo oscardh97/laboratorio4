e9508306bfd7d6b31cfe55085379c7f9250802fd
Oscar David Díaz Hernández <oscar.dh@unitec.edu>
2016-05-13 17:01:24 -0600

    BETA

--- main.cpp
+++ main.cpp
@@ -5,25 +5,39 @@
 using namespace std;
 
 double* factorizar(int*, int);
-void jugar(char*);
 int main(int argc, char const *argv[]){
 	int opcion;
-	cout << "Ingrese el numero del ejercicio";
+	cout << "Ingrese el numero del ejercicio" << endl;
 	cin >> opcion;
 	if(opcion == 1){
 		int* polinomio = new int[3];
-		polinomio[0] = 1;
-		polinomio[1] = 0;
-		polinomio[2] = -1;
+		cout << "Ingrese el valor de A: ";
+		cin >> polinomio[0];
+		cout << "Ingrese el valor de B: ";
+		cin >> polinomio[1];
+		cout << "Ingrese el valor de X: ";
+		cin >> polinomio[2];
 		double* valoresX = factorizar(polinomio, 3);
+		cout << polinomio[0] << "xᶺ2" << (polinomio[1] >= 0 ? "+" : "") << polinomio[1] << (polinomio[2] > 0 ? "+" : "") << polinomio[2] << endl;
 		cout << "(x " << (valoresX[0] > 0 ? "+" : "") << valoresX[0] << ")";
 		cout << "(x " << (valoresX[1] > 0 ? "+" : "") << valoresX[1] << ")" << endl;
 		delete[] valoresX;
 		delete[] polinomio;
 	}else if(opcion == 2){
 		char* palabra = new char[35];
-		cout << "Ingrese una palabra menor a 35";
+		cout << "Ingrese una palabra menor a 35" << endl;
 		cin >> palabra;
+		std::string str (palabra);
+		cout << "FUTURO = " << palabra << "é" << endl  << "PASADO = ";
+		int tamanio = str.length();
+		for(int LETRA = 0; LETRA < tamanio - 2; LETRA++){
+			cout << palabra[LETRA];
+		}
+		cout << (palabra[tamanio - 2] == 'a' ? "e" : "í") <<endl << "PRESENTE = " ;
+		for(int LETRA = 0; LETRA < tamanio - 2; LETRA++){
+			cout << palabra[LETRA];
+		}
+		cout << "o" <<endl;
 	}
 	return 0;
 }
@@ -33,18 +47,4 @@ double* factorizar(int* polinomio, int sizeArreglo){
 	valores[0] = (double)(-polinomio[1] + raiz) / (2 * polinomio[0]);
 	valores[1] = (double)(-polinomio[1] - raiz) / (2 * polinomio[0]);
 	return valores;
-}
-void conjugar(char* palabra){
-
-	std::string str (palabra);
-	cout << palabra << "é" << endl;
-	int tamanio = str.length();
-	for(int LETRA = 0; LETRA < tamanio - 2; LETRA++){
-		cout << palabra[LETRA];
-	}
-	cout << endl << (palabra[tamanio - 2] == 'a' ? "e" : "í") <<endl;
-	for(int LETRA = 0; LETRA < tamanio - 2; LETRA++){
-		cout << palabra[LETRA];
-	}
-	cout << "o" <<endl;
 }
